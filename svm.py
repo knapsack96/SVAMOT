a1f80bf9709275bde44192c9c5c6966182f114aa
knapsack96 <30894109+knapsack96@users.noreply.github.com>
2023-11-15 03:11:42 +0100

    Update svm.py

--- svm.py
+++ svm.py
@@ -3,15 +3,13 @@ from sklearn.svm import LinearSVC, SVC
 import pickle
 import numpy as np
 
-scaler = StandardScaler()
-
-#with open("moterpermatrack17train.pkl","rb") as f:
+#with open("moterpermatrack17train_data.pkl","rb") as f:
 #    train = pickle.load(f)
-#with open("moterpermatrack17.pkl","rb") as f:
+#with open("moterpermatrack17_data.pkl","rb") as f:
 #    test = pickle.load(f)
-with open("mot17.pkl","rb") as f:
+with open("outrackbytetrack_data17.pkl","rb") as f:
     train = pickle.load(f)
-with open("mot20.pkl","rb") as f:
+with open("outrackbytetrack_data20.pkl","rb") as f:
     test = pickle.load(f)
 
 X = np.array([x[1:3]+x[5:7]+x[11:13] for x in train])
@@ -28,7 +26,7 @@ clf.fit(X, y)
 sample = np.array([x[1:3]+x[5:7]+x[11:13] for x in test])
 
 label = clf.predict(sample)
-with open('svm0025bytetrackouttrack.pkl','wb') as f:
+ith open('svm0025bytetrackouttrack20.pkl','wb') as f:
     pickle.dump([h for h in zip([x[0] for x in test],label)], f)
 
 #with open('svmoterpermatrack17.pkl','wb') as f:
