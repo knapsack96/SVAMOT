c201ad6ffd16cf21f88b0065f81bb6b603a95531
knapsack96 <30894109+knapsack96@users.noreply.github.com>
2023-11-15 03:13:36 +0100

    Update make_file_from_svm_results.py

--- make_file_from_svm_results.py
+++ make_file_from_svm_results.py
@@ -2,7 +2,8 @@
 import pickle
 with open("svm0025bytetrackouttrack20.pkl","rb") as f:
     res = pickle.load(f)
-with open("MOT20-04","w") as f:
+#this code can be repeated for each sequence of test of a dataset
+with open("MOT20-04.txt","w") as f:
 #here you set the limits of all the frames belonging to the written sequence. Be careful, sometimes some frames are excluded from results, so check before.
     for k in ris[0:2080]: 
         f.write(str(k[0])+','+str(k[1])+'\n')
